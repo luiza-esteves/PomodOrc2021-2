9e389612c6a64d3dde1530f8e4f7099f90e07807
Pedro Henrique <pedrodsm1819@gmai.com>
2021-11-26 16:00:47 -0300

    Aba de adicionar listas e marcaçoes das listas

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,11 +2,32 @@ import React, { useState } from 'react';
 import * as C from "./AppStyle";
 import { Item } from './types/Item';
 import { ListItem } from './Components/ListItem';
+import { AddArea } from './Components/AddArea';
+import { listenerCount } from 'process';
 
 const App = () => {
   const [list, setList] = useState<Item[]>([
-    {id : 1, name : 'Tarefas de Casa', done : false}
   ]);
+
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
 
   return (
     <div>
@@ -14,10 +35,14 @@ const App = () => {
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
 
-          { /* Área de adicionar nova  Lista de tarefas */}
+          <AddArea onEnter={handleAddTask} />
 
           {list.map((item, index) =>(
-            <ListItem key={index} item={item}/>
+            <ListItem 
+            key={index} 
+            item={item}
+            onChange={handleTaskChange}
+            />
           ))}
 
         </C.Area>
--- front/src/Components/ListItem/index.tsx
+++ front/src/Components/ListItem/index.tsx
@@ -1,19 +1,22 @@
+import { useState } from 'react';
 import * as C from './styles';
 import { Item } from '../../types/Item';
 
 type Props = {
-    item: Item
+    item: Item,
+    onChange : (id: number, done: boolean) => void
 }
-export const ListItem = ({ item }: Props) => {
+
+export const ListItem = ({ item, onChange }: Props) => {
+
     return (
-        <C.Container>
+        <C.Container done={item.done}>
             <input 
-            type='text'
-            placeholder='Adicione uma Lista de Tarefas'
-            name='text'
-             />
-             <button>+</button>
-       
+                type="checkbox" 
+                checked={item.done}
+                 onChange={e => onChange(item.id, e.target.checked)}
+            />
+            <label>{item.name}</label>
         </C.Container>
     );
 }
