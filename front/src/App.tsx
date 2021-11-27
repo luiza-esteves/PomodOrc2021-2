116401cd3cebdbfc8d5210e0471e8a4262329e52
Luíza Esteves <luizaesteves987@gmail.com>
2021-11-26 22:04:35 -0300

    inclusao de novas tags na app

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import * as C from './App.styles';
 import {Item} from './types/Item';
 import {ListItem} from './components/listItem';
+import { AddArea } from './components/AddArea';
 
 const App = () =>{
   const [list,setList] = useState<Item[]>([
@@ -13,6 +14,7 @@ const App = () =>{
       <C.Area> 
         <C.Header>Lista de Tarefas</C.Header>
 
+        <AddArea />
 
         {list.map((item, index)=>(
           <ListItem key={index} item={item}/>
