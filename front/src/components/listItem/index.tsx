72e18fe36dde0a73b6358d305982ccad814062db
Luíza Esteves <luizaesteves987@gmail.com>
2021-11-28 21:27:47 -0300

    compoentes para editar lista

--- front/src/components/listItem/index.tsx
+++ front/src/components/listItem/index.tsx
@@ -1,6 +1,10 @@
 import * as C from './styles';
 import {Item} from  '../../types/Item';
 import { useState } from 'react';
+import {ReactComponent as CLose} from '../../assets/close.svg';
+import {ReactComponent as CLock} from '../../assets/clock.svg';
+import {ReactComponent as Check} from '../../assets/check.svg';
+import {ReactComponent as Edit} from '../../assets/edit.svg'
 
 type Props = {
     item: Item
@@ -10,13 +14,20 @@ export const ListItem = ({item}: Props) => {
     const[isChecked, setIsChecked] = useState(item.done);
     
     return (
+        
         <C.Container done={isChecked} className = "tarefa">
+            <Edit />
            <input 
             type="checkbox" 
             checked ={isChecked}
             onChange = {e => setIsChecked(e.target.checked)}
             />
-           <label> {item.name}</label>
+           <label className="tarefas"> 
+               {item.name} 
+                <CLose />
+                <CLock />
+                <Check />
+            </label>
         </C.Container>
     );
 }
